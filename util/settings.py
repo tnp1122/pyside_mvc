2bc120ee47aa6b75e16eac74708250e3cba10a17
준하 노 <tnptt1122@gmail.com>
2024-01-08 18:53:05 +0900

    refactor: Settings -> Util 디렉토리로 이동

--- util/settings.py
+++ util/settings.py
@@ -1,12 +1,12 @@
 from PySide6.QtCore import QSettings
 
 
-class SettingManager:
+class Settings:
     _instance = None
 
     def __new__(cls):
         if not cls._instance:
-            cls._instance = super(SettingManager, cls).__new__(cls)
+            cls._instance = super(Settings, cls).__new__(cls)
         return cls._instance
 
     def __init__(self):
@@ -14,20 +14,33 @@ class SettingManager:
             self.initialized = True
             self.settings = QSettings("Jen Life", "PIT")
 
-    def save_str(self, key, value):
+    def _save_str(self, key, value):
         self.settings.setValue(key, value)
 
-    def load_str(self, key):
+    def _load_str(self, key):
         return self.settings.value(key)
 
+    def _remove_str(self, key):
+        self.settings.remove(key)
+
     def save_access_token(self, value):
-        self.save_str("access_token", value)
+        self._save_str("access_token", value)
 
     def save_refresh_token(self, value):
-        self.save_str("refresh_token", value)
+        self._save_str("refresh_token", value)
 
     def load_access_token(self):
-        return self.load_str("access_token")
+        return self._load_str("access_token")
 
     def load_refresh_token(self):
-        return self.load_str("refresh_token")
+        return self._load_str("refresh_token")
+
+
+def main():
+    settings = Settings()
+
+    print(settings.load_access_token())
+
+
+if __name__ == "__main__":
+    main()
