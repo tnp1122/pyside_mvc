7c09791ce79e496d796b3b0bfea01ef00a8db819
준하 노 <tnptt1122@gmail.com>
2024-01-12 10:27:30 +0900

    feat: ImageButton Widget 추가

---
+++ ui/common/image_button.py
@@ -0,0 +1,38 @@
+from PySide6.QtCore import QSize
+from PySide6.QtGui import QPixmap, QIcon
+from PySide6.QtWidgets import QPushButton, QApplication
+
+
+class ImageButton(QPushButton):
+    def __init__(self, text=None, image=None, size=None, parent=None):
+        super().__init__(text, parent)
+
+        if image:
+            if isinstance(image, QPixmap):
+                pixmap = image
+            else:
+                pixmap = QPixmap(image)
+            self.setIcon(QIcon(pixmap))
+            if size:
+                icon_size = QSize(*size)
+            else:
+                icon_size = pixmap.rect().size()
+            self.setIconSize(icon_size)
+            self.setFixedHeight(icon_size.height())
+        self.setStyleSheet("""
+            QPushButton {
+                border: 0px;
+            }
+        """)
+
+
+def main():
+    app = QApplication([])
+    image = "../../static/image/cogwheel.png"
+    widget = ImageButton(image=image)
+    widget.show()
+    app.exec()
+
+
+if __name__ == "__main__":
+    main()
