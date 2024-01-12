24cc27e5819afd77adc8911bf3588c97d6ad35ff
준하 노 <tnptt1122@gmail.com>
2024-01-12 10:27:10 +0900

    feat: BaseWidget 추가

--- ui/common/base_widget.py
+++ ui/common/base_widget.py
@@ -1,12 +1,24 @@
 from PySide6.QtCore import Signal
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QWidget, QStackedWidget, QTabWidget
 
 
-class BaseWidgetView(QWidget):
+class BaseViewMixin:
     ui_initialized_signal = Signal()
-
-    def __init__(self, parent=None):
-        super().__init__(parent)
 
     def emit_ui_initialized_signal(self):
         self.ui_initialized_signal.emit()
+
+
+class BaseWidgetView(BaseViewMixin, QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+
+class BaseStackedWidgetView(BaseViewMixin, QStackedWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+
+class BaseTabWidgetView(BaseViewMixin, QTabWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
