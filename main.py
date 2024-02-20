7fc613057af8dc026cf057b706901578c3873792
준하 노 <tnptt1122@gmail.com>
2024-02-20 10:14:42 +0900

    feat: 로깅 매커니즘 수정

--- main.py
+++ main.py
@@ -1,22 +1,47 @@
+import logging
+import os
+import sys
+from datetime import datetime
+
+
+def logging_config():
+    current_directory = os.getcwd()
+    log_folder_path = os.path.join(current_directory, "log")
+    if not os.path.exists(log_folder_path):
+        os.makedirs(log_folder_path)
+
+    now = datetime.now()
+    now_str = now.strftime("%y%m%d_%H%M%S")
+    file_name = os.path.join(log_folder_path, now_str)
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+
+    file_handler = logging.FileHandler(file_name)
+    file_handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
+
+def exception_hook(exc_type, exc_value, exc_traceback):
+    logging.error("Unhandled exception", exc_info=(exc_type, exc_value, exc_traceback))
+
+
 def main():
     from dotenv import load_dotenv
-    import os
-    import logging
 
     from PySide6.QtWidgets import QApplication
     from ui.app import AppController
 
     load_dotenv()
-    file_name = os.getenv("LOG_FILE_NAME")
-
-    if file_name:
-        logging.basicConfig(filename=file_name, filemode="w")
-    else:
-        logging.basicConfig(
-            format='[%(levelname)s] %(asctime)s:%(message)s',
-            datefmt='%Y-%m-%d %I:%M:%S %p',
-            level=logging.DEBUG
-        )
     app = QApplication([])
     main_app = AppController()
     main_app.view.show()
@@ -24,4 +49,6 @@ def main():
 
 
 if __name__ == "__main__":
+    logging_config()
+    sys.excepthook = exception_hook
     main()
