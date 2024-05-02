aa07c7389b976f64e0f9e13d00dcc40aa1d72c8c
준하 노 <tnptt1122@gmail.com>
2024-05-02 11:22:35 +0900

    chore: 파일 로깅 제거
    - 버전 관리 실행기에서 로깅 통합 관리

--- main.py
+++ main.py
@@ -12,25 +12,16 @@ def logging_config():
     if not os.path.exists(log_folder_path):
         os.makedirs(log_folder_path)
 
-    now = datetime.now()
-    now_str = now.strftime("%y%m%d_%H%M%S")
-    file_name = os.path.join(log_folder_path, now_str)
-
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
 
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
 
-    file_handler = logging.FileHandler(file_name)
-    file_handler.setLevel(logging.INFO)
-
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     console_handler.setFormatter(formatter)
-    file_handler.setFormatter(formatter)
 
     logger.addHandler(console_handler)
-    logger.addHandler(file_handler)
 
 
 def exception_hook(exc_type, exc_value, exc_traceback):
